d694f125e452d2c46493675be0a6b978247b776a
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-05-30 20:57:59 +0530

    Time: 4 ms (49.88%), Space: 6.1 MB (8.91%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0||n==1){
+            return n;
+        }
+        int *a=new int[n+1];
+        a[0]=0;
+        a[1]=1;
+        for(int i=2;i<=n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n];
+        
+    }
+};
