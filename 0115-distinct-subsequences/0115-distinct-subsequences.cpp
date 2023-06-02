ec651da98be29386b2fe0a486446a04990d355e9
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-06-03 00:35:35 +0530

    Time: 30 ms (87.64%), Space: 10.6 MB (83.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    unsigned dp[1001][1001];
     int numDistinct(string s, string t) {
-        int m = s.size(), n = t.size();
-        for(int i = 0; i < m; i++) dp[i][0] = 1;
+        int n = s.size(), m = t.size();
+        unsigned dp[1001][1001] = {0};
 
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
                 dp[i][j] = (s[i-1] == t[j-1]) ? dp[i-1][j-1] + dp[i-1][j] : dp[i-1][j];
             }
         }  
-        return dp[m][n];
+        return dp[n][m];
     }
 };
