b690fecc5f7a6871485692dfa641d53a0ce23739
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-06-05 17:47:01 +0530

    Time: 24 ms (81.12%), Space: 28.4 MB (14.81%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,29 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+// struct TreeNode {
+//     int val;
+//     TreeNode* left;
+//     TreeNode* right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+// };
+
 class Solution {
-public:
-int maxpath(TreeNode* root,int &maxi){
-    if(root==NULL){
-        return 0;
+    int maxPathSum_util(TreeNode* root, int& result) {
+        if (!root)
+            return 0;
+
+        int left = maxPathSum_util(root->left, result);
+        int right = maxPathSum_util(root->right, result);
+
+        int max_straight = std::max(std::max(left, right) + root->val, root->val);
+        int max_caseVal = std::max(max_straight, left + right + root->val);
+        result = std::max(max_caseVal, result); // CASE-3
+
+        return max_straight;
     }
-    int left=max(0,maxpath(root->left,maxi));
-    int right=max(0,maxpath(root->right,maxi));
-    maxi=max(maxi,left+right+root->val);
-    return max(left,right)+root->val;
-}
+
+public:
     int maxPathSum(TreeNode* root) {
-       int maxi=INT_MIN;
-       maxpath(root,maxi);
-       return maxi;
-        
+       
+        int result = INT_MIN;
+        maxPathSum_util(root, result);
+        return result;
     }
 };
