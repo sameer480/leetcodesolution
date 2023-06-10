627c469ebe8f8eb8bbe27c02d64668dcef5bf1fc
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-06-10 18:30:44 +0530

    Time: 0 ms (100%), Space: 6.4 MB (59.9%) - LeetHub

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        long long mod = 1e9 + 7;
+        vector<long long> v(1001,0);
+        v[1]=1;
+        v[2]=2;
+        v[3]=5;
+        if(n<=3){
+            return v[n];
+        }
+        for(int i=4;i<=n;i++){
+            v[i]=2*v[i-1]+v[i-3];
+            v[i]%=mod;
+        }
+        return v[n];
+        
+    }
+};
